0c3e5b060f59b788974a5e54b759aa851e331d12
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2026-02-15 13:21:24 +0500

    feat: yangi Flutter ilovasi yaratildi

---
+++ lib/src/application.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+    );
+  }
+}
